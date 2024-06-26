afd55db3e284b58d866d5396273047df048689a1
Cameron <cbogatez@gmail.com>
2024-06-26 14:48:03 +1000

    Testing mixpanel

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,10 @@
+import mixpanel from 'mixpanel-browser';
+
 console.log("hello world");
+
+mixpanel.init("f88e6c156550ad720ed768bb3742664c");
+console.log('mixpanel initialized');
+mixpanel.track("test");
 
 // DOMready has fired
 // May now use jQuery and Webflow API
