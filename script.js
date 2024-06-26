7d344d887e400739d1794ee8e09a1539284caa51
Cameron <cbogatez@gmail.com>
2024-06-26 15:45:48 +1000

    Style and clean up

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 //import mixpanel from 'mixpanel-browser';
 
-console.log("hello world");
 
 mixpanel.init("f88e6c156550ad720ed768bb3742664c");
-console.log('mixpanel initialized');
-mixpanel.track("test");
-
-// DOMready has fired
-// May now use jQuery and Webflow API
 
 var params = new URLSearchParams(window.location.search);
 var page = params.get('p');
 
-var pageTitle = document.title;
-mixpanel.track("pageview." + page);
+mixpanel.track("test.pageview." + page);
